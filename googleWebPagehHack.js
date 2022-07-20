b21e782b64c37ad5cb797ab773e962123124a199
Shubham <shubham.extension.developer@gmail.com>
2022-07-20 13:22:11 +0100

    [v2022.7] Fixed the Google Webpage issue where content menu box was not coming on right click

--- googleWebPagehHack.js
+++ googleWebPagehHack.js
@@ -1,3 +1,7 @@
 document.querySelectorAll('[data-sokoban-container]').forEach((elem) => {
     elem.style.contain = "none";
 });
+
+document.querySelectorAll('[data-header-feature]').forEach((elem) => {
+    elem.style.contain = "none";
+});
